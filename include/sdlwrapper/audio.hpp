b0066f7208eb216585e99b5682c137d19dfa3aae
Cory Sherman <coryshrmn@gmail.com>
2017-09-22 11:51:23 -0700

    add AudioDevice status, lock, and obtained spec

--- include/sdlwrapper/audio.hpp
+++ include/sdlwrapper/audio.hpp
@@ -77,19 +77,76 @@ private:
     std::uint8_t _channels {};
 };
 
+enum class AudioSpecChanges : int
+{
+    FREQUENCY = SDL_AUDIO_ALLOW_FREQUENCY_CHANGE,
+    FORMAT = SDL_AUDIO_ALLOW_FORMAT_CHANGE,
+    CHANNELS = SDL_AUDIO_ALLOW_CHANNELS_CHANGE,
+    ANY = SDL_AUDIO_ALLOW_ANY_CHANGE
+};
+
 class AudioDevice
 {
 public:
     using Callback = std::function<void(std::uint8_t* stream, int len)>;
 
-    AudioDevice(const AudioSubsystem&, const char* name, bool capture, int freq, AudioFormat format, std::uint8_t channels, std::uint16_t samples, Callback callback, int allowedChanges = 0);
-    AudioDevice(const AudioSubsystem&, const char* name, bool capture, int freq, AudioFormat format, std::uint8_t channels, std::uint16_t samples, SDL_AudioCallback callback, void* userData = nullptr, int allowedChanges = 0);
+    /**
+     * @brief Open an audio device with a std::function callback.
+     *
+     * @param name  Device name
+     * @param capture  Open a recording device instead of an output device
+     * @param freq  Sample rate in Hz
+     * @param format  Format for each sample
+     * @param channels  Number of audio channels
+     * @param samples  Buffer size in samples
+     * @param callback  Function to call when audio is required (for output devices) or available (for capture devices)
+     * @param allowedChanges  Any of the AudioSpecChanges bit flags OR'd together.
+     */
+    AudioDevice(const AudioSubsystem&, const char* name, bool capture, int freq, AudioFormat format, std::uint8_t channels, std::uint16_t samples, Callback callback, AudioSpecChanges allowedChanges = {});
+
+    /**
+     * @brief Open an audio device with raw SDL_AudioCallback and UserData.
+     *
+     * @param name  Device name
+     * @param capture  Open a recording device instead of an output device
+     * @param freq  Sample rate in Hz
+     * @param format  Format for each sample
+     * @param channels  Number of audio channels
+     * @param samples  Buffer size in samples
+     * @param callback  Function to call when audio is required (for output devices) or available (for capture devices)
+     * @param userData  Passed to callback.
+     * @param allowedChanges  Any of the AudioSpecChanges bit flags OR'd together.
+     */
+    AudioDevice(const AudioSubsystem&, const char* name, bool capture, int freq, AudioFormat format, std::uint8_t channels, std::uint16_t samples, SDL_AudioCallback callback, void* userData = nullptr, AudioSpecChanges allowedChanges = {});
+
+    /**
+     * @brief Open an audio device with no callback, to be used with queue or deque.
+     *
+     * @param name  Device name
+     * @param capture  Open a recording device instead of an output device
+     * @param freq  Sample rate in Hz
+     * @param format  Format for each sample
+     * @param channels  Number of audio channels
+     * @param samples  Buffer size in samples
+     * @param allowedChanges  Any of the AudioSpecChanges bit flags OR'd together.
+     */
+    AudioDevice(const AudioSubsystem&, const char* name, bool capture, int freq, AudioFormat format, std::uint8_t channels, std::uint16_t samples, AudioSpecChanges allowedChanges = {});
+
+    SDL_AudioStatus getStatus() const;
+
+    const SDL_AudioSpec& getObtainedSpec() const;
 
     void play();
     void pause();
 
+    void lock();
+    void unlock();
+
+    static int getNumAudioDevices(const AudioSubsystem&, bool capture = false);
+    static const char* getAudioDeviceName(const AudioSubsystem&, int index, bool capture = false);
+
 private:
-    void init(const char* name, bool capture, const SDL_AudioSpec& desiredSpec, int allowedChanges);
+    void init(const char* name, bool capture, const SDL_AudioSpec& desiredSpec, AudioSpecChanges allowedChanges);
 
     std::optional<Callback> _optCallback {};
     cwrapper::Resource<SDL_AudioDeviceID, detail::AudioDeviceDeleter> _resource {};
@@ -151,7 +208,7 @@ const uint8_t* Wav::end() const
     return _resource.getHandle() + _sizeBytes;
 }
 
-AudioDevice::AudioDevice(const AudioSubsystem&, const char *name, bool capture, int freq, AudioFormat format, uint8_t channels, uint16_t samples, AudioDevice::Callback callback, int allowedChanges)
+AudioDevice::AudioDevice(const AudioSubsystem&, const char *name, bool capture, int freq, AudioFormat format, uint8_t channels, uint16_t samples, AudioDevice::Callback callback, AudioSpecChanges allowedChanges)
     : _optCallback(callback)
 {
     SDL_AudioSpec desiredSpec {};
@@ -166,7 +223,7 @@ AudioDevice::AudioDevice(const AudioSubsystem&, const char *name, bool capture, 
     init(name, capture, desiredSpec, allowedChanges);
 }
 
-AudioDevice::AudioDevice(const AudioSubsystem&, const char *name, bool capture, int freq, AudioFormat format, uint8_t channels, uint16_t samples, SDL_AudioCallback callback, void *userdata, int allowedChanges)
+AudioDevice::AudioDevice(const AudioSubsystem&, const char *name, bool capture, int freq, AudioFormat format, uint8_t channels, uint16_t samples, SDL_AudioCallback callback, void *userdata, AudioSpecChanges allowedChanges)
 {
     SDL_AudioSpec desiredSpec {};
     desiredSpec.freq = freq;
@@ -177,6 +234,29 @@ AudioDevice::AudioDevice(const AudioSubsystem&, const char *name, bool capture, 
     desiredSpec.userdata = userdata;
 
     init(name, capture, desiredSpec, allowedChanges);
+}
+
+AudioDevice::AudioDevice(const AudioSubsystem &, const char *name, bool capture, int freq, AudioFormat format, uint8_t channels, uint16_t samples, AudioSpecChanges allowedChanges)
+{
+    SDL_AudioSpec desiredSpec {};
+    desiredSpec.freq = freq;
+    desiredSpec.format = format;
+    desiredSpec.channels = channels;
+    desiredSpec.samples = samples;
+
+    init(name, capture, desiredSpec, allowedChanges);
+}
+
+SDL_AudioStatus AudioDevice::getStatus() const
+{
+    assert(_resource.hasHandle());
+    return SDL_GetAudioDeviceStatus(_resource.getHandle());
+}
+
+const SDL_AudioSpec& AudioDevice::getObtainedSpec() const
+{
+    assert(_resource.hasHandle());
+    return _obtainedSpec;
 }
 
 void AudioDevice::play()
@@ -191,9 +271,35 @@ void AudioDevice::pause()
     SDL_PauseAudioDevice(_resource.getHandle(), true);
 }
 
-void AudioDevice::init(const char *name, bool capture, const SDL_AudioSpec& desiredSpec, int allowedChanges)
+void AudioDevice::lock()
 {
-    _resource.setHandle(SDL_OpenAudioDevice(name, capture, &desiredSpec, &_obtainedSpec, allowedChanges));
+    assert(_resource.hasHandle());
+    SDL_LockAudioDevice(_resource.getHandle());
+}
+
+void AudioDevice::unlock()
+{
+    assert(_resource.hasHandle());
+    SDL_UnlockAudioDevice(_resource.getHandle());
+}
+
+int AudioDevice::getNumAudioDevices(const AudioSubsystem&, bool capture)
+{
+    return SDL_GetNumAudioDevices(capture);
+}
+
+const char* AudioDevice::getAudioDeviceName(const AudioSubsystem&, int index, bool capture)
+{
+    const char* name = SDL_GetAudioDeviceName(index, capture);
+    if(name == nullptr) {
+        throw std::runtime_error(SDL_GetError());
+    }
+    return name;
+}
+
+void AudioDevice::init(const char *name, bool capture, const SDL_AudioSpec& desiredSpec, AudioSpecChanges allowedChanges)
+{
+    _resource.setHandle(SDL_OpenAudioDevice(name, capture, &desiredSpec, &_obtainedSpec, static_cast<int>(allowedChanges)));
     if(!_resource.hasHandle()) {
         throw std::runtime_error(SDL_GetError());
     }
@@ -205,7 +311,17 @@ void AudioDevice::dispatchCallback(void *userdata, uint8_t *stream, int len)
     reinterpret_cast<Callback*>(userdata)->operator()(stream, len);
 }
 
-
 } // namespace sdlwrapper
+
+namespace cwrapper
+{
+
+template <>
+struct EnumTraits<sdlwrapper::AudioSpecChanges>
+{
+    constexpr static bool isBitFlag = false;
+};
+
+} // namespace cwrapper
 
 #endif // SDLWRAPPER_AUDIO_HPP
